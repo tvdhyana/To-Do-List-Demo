592cca06e2f9edf2ceb8f7ad6c04bc969509c705
tvdhyana <dhyana@techvoot.com>
2022-06-20 19:16:35 +0530

    To- Do List

---
+++ To-do-list.js
@@ -0,0 +1,5 @@
+/* Select all the necessary Elements  */
+var input = document.querySelector(".todo_input");
+var MainTodoContainer = document.getElementById("todos");
+var addingButton = document.querySelector(".add-item");
+var deleteAllBtn = document.querySelector(".deleteBtn");
